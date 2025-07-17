3849d0081d211d9ff8b5d925c94d34d6539945fc
AbeloByte <gobeztech20@gmail.com>
2025-07-17 15:19:57 +0300

    add : Hero Section and Searching functionality with a reset and search Button

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,17 +1,55 @@
-import SearchForm from "../components/SearchForm";
+import { auth } from "@/auth";
+import SearchForm from "../../components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Abelo" },
+      _id: 1,
+      description: "This is a sample startup pitch",
+      image:
+        "https://images.unsplash.com/photo-1643299397136-a6cf89431e19?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      category: "Tech",
+      title: "Innovative Tech Startup",
+    },
+  ];
+  const query = (await searchParams).query;
   return (
-    <section className="pink_container">
-      <h2 className="heading">
-        Pitch Your Startup , <br /> Connect with Entrepreneurs
-      </h2>
+    <>
+      <section className="pink_container">
+        <h2 className="heading">
+          Pitch Your Startup , <br /> Connect with Entrepreneurs
+        </h2>
 
-      <p className="sub-heading !max-w-3xl">
-        Submit Ideas , Vote on Pitches , and Get Noticed in Virtual Competitons
-      </p>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas , Vote on Pitches , and Get Noticed in Virtual
+          Competitons
+        </p>
 
-      <SearchForm />
-    </section>
+        <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Searching results for: ${query}` : `All Startups`}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post._id} post={post} />
+            ))
+          ) : (
+            <p>No startups found.</p>
+          )}
+        </ul>
+      </section>
+    </>
   );
 }
